a0461ff1a05dae87bb4777ae2592561b05b81067
Mathias <gamingmcseries@gmail.com>
2022-09-15 20:43:22 +0200

    + Added Exercise 5

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -8,6 +8,7 @@
             bool ex2 = false;
             bool ex3 = false;
             bool ex4 = false;
+            bool ex5 = false;
             #region Exercise 1 - String valuses
             while (ex1)
             {
@@ -28,6 +29,7 @@
                 Console.Clear();
             }
             #endregion
+
             #region Exercise 2 - IF/ELSE statements
             while (ex2)
             {
@@ -97,6 +99,96 @@
                 Console.ReadLine();
                 Console.Clear();
 
+            }
+            #endregion
+
+            #region Exercise 5 - New Class file and use data from it
+            while (ex5)
+            {
+                
+                Console.WriteLine("Hej og velkommen :D");
+                Console.WriteLine("Dette er en tidlig version af en lommeregner.");
+                Console.WriteLine();
+                Console.WriteLine("Vælg en af de nedrestående muligheder:");
+                Console.WriteLine();
+
+                Console.WriteLine("1. Plus");
+                Console.WriteLine("2. Minus");
+                Console.WriteLine("3. Gange");
+                Console.WriteLine("4. Divider");
+
+                string input = Console.ReadLine();
+
+                int res;
+                int.TryParse(input, out res);
+
+                Calculator cal = new Calculator();
+
+                int input1;
+                int input2;
+
+                switch (res)
+                {
+                    case 1:
+
+                        Console.WriteLine("Du har valgt at pluse tald sammen");
+                        Console.WriteLine();
+                        Console.WriteLine("Indtast den første værdri: ");
+
+                        input1 = int.Parse(Console.ReadLine());
+                        
+                        Console.WriteLine("Indtast den anden værdi værdri: ");
+
+                        input2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Den sammlede værdi af "+ input1 + " + " + input2 + " er: " + cal.Add(input1, input2));
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Du har valgt at minus nogle tald");
+                        Console.WriteLine();
+                        Console.WriteLine("Indtast den første værdri: ");
+
+                        input1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Indtast den anden værdi værdri: ");
+
+                        input2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Den sammlede værdi af " + input1 + " - " + input2 + " er: " + cal.Subtract(input1, input2));
+                        break;
+                    case 3:
+                        Console.WriteLine("Du har valgt at gange nogle tald");
+                        Console.WriteLine();
+                        Console.WriteLine("Indtast den første værdri: ");
+
+                        input1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Indtast den anden værdi værdri: ");
+
+                        input2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Den sammlede værdi af " + input1 + " * " + input2 + " er: " + cal.Subtract(input1, input2));
+                        break;
+                    case 4:
+                        Console.WriteLine("Du har valgt at dividere nogle tald");
+                        Console.WriteLine();
+                        Console.WriteLine("Indtast den første værdri: ");
+
+                        input1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Indtast den anden værdi værdri: ");
+
+                        input2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Den sammlede værdi af " + input1 + " / " + input2 + " er: " + cal.Subtract(input1, input2));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid value - Try again");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
             }
             #endregion
         }
